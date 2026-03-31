a9949fcc045bc56e50ea3753276b48bcb2a0bc03
LarsSchoenemannDev <lars.dev90@gmail.com>
2026-03-31 17:03:14 +0200

    add small chicken

--- models/smallChicken.class.js
+++ models/smallChicken.class.js
@@ -1,30 +1,30 @@
-class Chicken extends MovableObject {
+class SmallChicken extends MovableObject {
 
     y = 366;
     height = 55
     width = 70
     imagesWalking = [
-        "./img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
-        "./img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
-        "./img/3_enemies_chicken/chicken_normal/1_walk/3_w.png"
+        "./img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
+        "./img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
+        "./img/3_enemies_chicken/chicken_small/1_walk/3_w.png"
     ]
 
     imageDead = [
-        "img/3_enemies_chicken/chicken_normal/2_dead/dead.png"
+        "img/3_enemies_chicken/chicken_small/2_dead/dead.png"
     ];
 
     constructor(x) {
         super();
-        this.loadImage("./img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
+        this.loadImage("./img/3_enemies_chicken/chicken_small/1_walk/1_w.png");
         this.loadImages(this.imagesWalking);
         this.loadImage(this.imageDead);
         this.x = x + 400 + Math.random() * 300;
         this.speed = 0.15 + Math.random() * 0.25;
         this.animate();
-        this.hitboxOffsetX = 25;
-        this.hitboxOffsetY = 25;
-        this.hitboxWidth = 50;
-        this.hitboxHeight = 50;
+        this.hitboxOffsetX = 25; 
+        this.hitboxOffsetY = 25;  
+        this.hitboxWidth = 50;    
+        this.hitboxHeight = 50;  
     }
 
 
